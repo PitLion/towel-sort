48c6049aaf02ab496d95ed584b62317a3516843f
Piotr Bausiuk <94122139+PitLion@users.noreply.github.com>
2022-08-07 16:02:30 +0300

    feat: add file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.exports = function towelSort (matrix) {
+
+  let arr = [];
+
+  if (typeof matrix === 'undefined') {
+    return arr;
+  } else {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let columnIdx = i % 2 === 0 ? j :  (matrix[i].length - j - 1);
+        arr.push(matrix[i][columnIdx]);
+      }
+    }
+    return arr;
+  }
+}
